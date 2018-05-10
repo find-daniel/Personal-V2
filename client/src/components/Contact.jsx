5492a09ef6210e4399c6d066f67870db1d122f4b
Daniel Sanchez <atlow.archive@gmail.com>
2018-05-09 23:22:41 -0700

    [content] add contact info

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -4,7 +4,20 @@ import React from 'react';
 
 const Contact = () => (
   <section className="contact-content section">
-    <h1>Inside Contact</h1>
+    <div className="contact-info">
+      <p>
+        I'm currently open to new opportunities! Feel free to shoot me an email if you wanna chat or let me know how I
+        can help you.
+      </p>
+    </div>
+    <div className="contact-options">
+      <div>
+        Email: <a href="mailto:atlow.archive@gmail.com?Subject=Hello%20Daniel">atlow.archive@gmail.com</a>
+      </div>
+      <div>
+        Phone: <a href="tel:+1-951-281-9420">951-281-9420</a>
+      </div>
+    </div>
   </section>
 );
 
