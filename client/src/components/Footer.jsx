596f435d5d9086788a1b3aab983c30f7d3f1de62
Daniel Sanchez <atlow.archive@gmail.com>
2018-05-10 17:34:38 -0700

    [footer] add social icons

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,9 +4,55 @@ import React from 'react';
 
 const Footer = () => (
   <section className="footer-content footer">
-    <div className="socials">#</div>
+    <div className="socials">
+      <div>
+        <a
+          className="social-icon"
+          href="https://www.linkedin.com/in/danielsanr/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin-in fa-lg" />
+        </a>
+      </div>
+      <div>
+        <a className="social-icon" href="https://github.com/find-daniel" target="_blank" rel="noopener noreferrer">
+          <span className="fab fa-github-alt fa-lg" />
+        </a>
+      </div>
+      <div>
+        <a
+          className="social-icon"
+          href="https://www.facebook.com/danielsanchez.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-facebook-f fa-lg" />
+        </a>
+      </div>
+      <div>
+        <a
+          className="social-icon"
+          href="https://www.instagram.com/mr.danielsanchez/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-instagram fa-lg" />
+        </a>
+      </div>
+      <div>
+        <a
+          className="social-icon"
+          href="https://open.spotify.com/user/126537593?si=i7M-hV82Tsq3g6Q6HVmuGQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-spotify fa-lg" />
+        </a>
+      </div>
+    </div>
     <div>
-      <p>&copy; Daniel Sanchez 2018</p>
+      <p className="footer-info">Made by DANIEL SANCHEZ 2018</p>
     </div>
   </section>
 );
--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -7,32 +7,41 @@ const Portfolio = () => (
     <div className="portfolio-item">
       <div className="item-header">
         <h4>Wayfare</h4>
+        <p>Fullstack Engineer</p>
       </div>
       <div className="item-img">IMAGE</div>
       <div className="item-info">
         <p>Work-stay app for travelers looking to barter skills/services in return for lodging.</p>
       </div>
-      <button className="custom-btn">See the Code</button>
+      <a href="https://github.com/find-daniel/wayfare-ui" target="_blank" rel="noopener noreferrer">
+        <button className="custom-btn">See the Code</button>
+      </a>
     </div>
     <div className="portfolio-item">
       <div className="item-header">
         <h4>Codesling.io</h4>
+        <p>Fullstack Engineer</p>
       </div>
       <div className="item-img">IMAGE</div>
       <div className="item-info">
         <p>Typeracer for algo challenges - see who can solve the problem first!</p>
       </div>
-      <button className="custom-btn">See the Code</button>
+      <a href="https://github.com/find-daniel/codesling.io-ui" target="_blank" rel="noopener noreferrer">
+        <button className="custom-btn">See the Code</button>
+      </a>
     </div>
     <div className="portfolio-item">
       <div className="item-header">
         <h4>FoodThoughts</h4>
+        <p>Fullstack Engineer</p>
       </div>
       <div className="item-img">IMAGE</div>
       <div className="item-info">
         <p>Platform for searching and reviewing restaurants.</p>
       </div>
-      <button className="custom-btn">See the Code</button>
+      <a href="https://github.com/find-daniel/Food-Thoughts" target="_blank" rel="noopener noreferrer">
+        <button className="custom-btn">See the Code</button>
+      </a>
     </div>
   </section>
 );
