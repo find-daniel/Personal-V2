074cb64abeb7b6d0d9233784372b22be2b36bc6a
Daniel Sanchez <atlow.archive@gmail.com>
2018-08-16 16:30:49 -0700

    [linting] clean up old imports

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -3,11 +3,6 @@ import Loadable from 'react-loadable';
 import React from 'react';
 import Banner from './Banner';
 import SectionHeader from './SectionHeader';
-// import About from './About';
-// import Tech from './Tech';
-// import Portfolio from './Portfolio';
-// import Contact from './Contact';
-// import Footer from './Footer';
 import Nav from './Nav';
 
 // Lazy loading
@@ -43,18 +38,13 @@ const Content = () => (
     <Nav />
     <Banner />
     <SectionHeader section="ABOUT" id="about" />
-    {/* <About /> */}
     <AboutLoad />
     <SectionHeader section="SOME TECH I ENJOY USING" id="tech" />
-    {/* <Tech /> */}
     <TechLoad />
     <SectionHeader section="PORTFOLIO" id="portfolio" />
-    {/* <Portfolio /> */}
     <PortfolioLoad />
     <SectionHeader section="CONTACT" id="contact" />
-    {/* <Contact /> */}
     <ContactLoad />
-    {/* <Footer /> */}
     <FooterLoad />
   </div>
 );
